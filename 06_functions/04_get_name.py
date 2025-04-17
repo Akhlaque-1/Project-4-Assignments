c5dec9a1154dd4dc0d874b1a9756856a62af52d7
Akhlaque-1 <161037198+Akhlaque-1@users.noreply.github.com>
2025-04-17 20:37:45 +0500

    Add files via upload

---
+++ 06_functions/04_get_name.py
@@ -0,0 +1,11 @@
+def get_name():
+    return "Ali"
+
+# There is no need to edit code beyond this point
+
+def main():
+    name = get_name() # get_name() will return a string which we store to the 'name' variable here
+    print("Bhai", name, "! 🤠")
+
+if __name__ == '__main__':
+    main()
