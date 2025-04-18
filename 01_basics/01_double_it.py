b2f87135f3c27d7830dd7dcc2cdee5974feec802
Akhlaque-1 <161037198+Akhlaque-1@users.noreply.github.com>
2025-04-18 13:29:53 +0500

    Add files via upload

---
+++ 01_basics/01_double_it.py
@@ -0,0 +1,38 @@
+def main():
+    # Ask the user to enter a number
+    user_input = int(input("Enter a number: "))
+    
+    # Start with the doubled value
+    curr_value = user_input * 2
+    
+    # Keep doubling and printing while curr_value is less than 100
+    while curr_value < 100:
+        print(curr_value)
+        curr_value = curr_value * 2
+
+    # Print the last value which made it >= 100
+    print(curr_value)
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
+def main():
+    # Ask the user to enter a number
+    user_input = int(input("Enter a number: "))
+    
+    # Start with the doubled value
+    curr_value = user_input * 2
+    
+    # Keep doubling and printing while curr_value is less than 100
+    while curr_value < 100:
+        print(curr_value)
+        curr_value = curr_value * 2
+
+    # Print the last value which made it >= 100
+    print(curr_value)
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
